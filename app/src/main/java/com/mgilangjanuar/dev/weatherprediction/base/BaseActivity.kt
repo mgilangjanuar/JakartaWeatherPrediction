71ce77bf6bc21a055412281c24baab01ce9fc02b
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2018-02-12 18:00:59 +0700

    remove unused function

--- app/src/main/java/com/mgilangjanuar/dev/weatherprediction/base/BaseActivity.kt
+++ app/src/main/java/com/mgilangjanuar/dev/weatherprediction/base/BaseActivity.kt
@@ -2,7 +2,6 @@ package com.mgilangjanuar.dev.weatherprediction.base
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
@@ -11,7 +10,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 
 /**
  * Created by mgilangjanuar (mgilangjanuar@gmail.com)
@@ -35,23 +33,11 @@ abstract class BaseActivity : AppCompatActivity() {
         }
     }
 
-    fun showSnackbar(@StringRes text: Int) {
-        showSnackbar(getString(text))
-    }
-
     fun showSnackbar(message: String) {
         val view = findViewById<View>(android.R.id.content)
         val snack = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
         val textView = snack.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
         textView.setTextColor(Color.WHITE)
         snack.show()
-    }
-
-    fun showToast(@StringRes text: Int) {
-        showToast(getString(text))
-    }
-
-    fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
